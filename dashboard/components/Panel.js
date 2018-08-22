83635df2f4e616edc4472554003c9682f3805935
Mark Markov <markymarkov@gmail.com>
2018-08-22 17:37:41 +0300

    Added parcel and script to run it.

---
+++ dashboard/components/Panel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Panel = () => {
+  return <h1>This is now completely different</h1>;
+};
+
+export default Panel;
---
+++ dashboard/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Panel from './components/Panel';
+
+
+ReactDOM.render(<Panel />, document.getElementById('root'));
